94536cd599c85ce48fdeb23041b37125c407e4ab
Siwan Li <siwanli1337@gmail.com>
2023-09-13 13:10:10 +1000

    Added adjacency list function

--- searcher/searcher.java
+++ searcher/searcher.java
@@ -9,13 +9,12 @@ public class searcher {
         List<String> countries = new ArrayList<>();
 
         try {
-            File test = new File(inputFile);
-            Scanner sc = new Scanner(test);
+            Scanner sc = new Scanner(new File(inputFile));
             String code = sc.nextLine();
             while (sc.hasNext())
             {
                 String[] line = sc.nextLine().split(",");
-                String newCode = line[1];
+                String newCode = line[0];
                 if (!newCode.equals(code)) {
                     countries.add(newCode);
                     code = newCode;
@@ -28,9 +27,30 @@ public class searcher {
         return countries;
     }
 
+    public static Map<String, List<String>> adjacencyList() {
+        String inputFile = "GEODATASOURCE-COUNTRY-BORDERS.CSV";
+        Map<String, List<String>> list = new HashMap<>();
+        List<String> countries = index();
+        for (String code : countries)
+            list.put(code, new ArrayList<>());
+
+        try {
+            Scanner sc = new Scanner(new File(inputFile));
+            String code = sc.nextLine();
+            while (sc.hasNext())
+            {
+                String[] line = sc.nextLine().split(",");
+                list.get(line[0]).add(line[2]);
+            }
+            sc.close();
+        } catch(Exception e) {
+            System.out.println("didn't work :((");
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
-        List<String> result = index();
-        System.out.println(result.size());
-        System.out.println(result);
+        Map<String, List<String>> list = adjacencyList();
+        System.out.print(list);
     }
 }
